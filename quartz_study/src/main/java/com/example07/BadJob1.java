56671790032a21b3b33848a426b7bfd4a4e7b44a
yuleiqq <18601911066@163.com>
2020-07-16 18:05:16 +0800

    删除多余注释

--- quartz_study/src/main/java/com/example07/BadJob1.java
+++ quartz_study/src/main/java/com/example07/BadJob1.java
@@ -1,20 +1,3 @@
-/* 
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved. 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy 
- * of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
- * under the License.
- * 
- */
- 
 package com.example07;
 
 import org.quartz.DisallowConcurrentExecution;
@@ -29,13 +12,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
-/**
- * <p>
- * A job dumb job that will throw a job execution exception
- * </p>
- * 
- * @author Bill Kratzer
- */
 @PersistJobDataAfterExecution
 @DisallowConcurrentExecution
 public class BadJob1 implements Job {
