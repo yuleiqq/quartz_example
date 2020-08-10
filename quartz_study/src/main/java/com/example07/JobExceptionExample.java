6cd1f2d5155af1e39605475aa4c0ebae99962a75
yulei <yul41@chinaunicom.cn>
2020-08-10 09:59:58 +0800

    注释修改

--- quartz_study/src/main/java/com/example07/JobExceptionExample.java
+++ quartz_study/src/main/java/com/example07/JobExceptionExample.java
@@ -1,20 +1,5 @@
-/* 
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved. 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy 
- * of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
- * under the License.
- * 
- */
- 
+
+
 package com.example07;
 
 import static org.quartz.DateBuilder.nextGivenSecondDate;
@@ -34,10 +19,9 @@ import org.slf4j.LoggerFactory;
 import java.util.Date;
 
 /**
- * This job demonstrates how Quartz can handle JobExecutionExceptions that are thrown by jobs.
- * 
- * @author Bill Kratzer
+ * 任务异常验证
  */
+
 public class JobExceptionExample {
 
   public void run() throws Exception {
